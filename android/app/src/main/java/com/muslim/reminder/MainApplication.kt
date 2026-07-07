d1ec4a689e6f087c1ef00c0cb5f8ee4f72678b28
kurmuz <acc4game05@gmail.com>
2026-07-07 11:36:11 +0300

    fix: add try-catch in MainApplication, disable Hermes

--- android/app/src/main/java/com/muslim/reminder/MainApplication.kt
+++ android/app/src/main/java/com/muslim/reminder/MainApplication.kt
@@ -42,12 +42,23 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+    try {
+      SoLoader.init(this, OpenSourceMergedSoMapping)
+    } catch (e: Throwable) {
+      android.util.Log.e("MainApp", "SoLoader init failed", e)
     }
-    ApplicationLifecycleDispatcher.onApplicationCreate(this)
+    try {
+      if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+        load()
+      }
+    } catch (e: Throwable) {
+      android.util.Log.e("MainApp", "NewArch load failed", e)
+    }
+    try {
+      ApplicationLifecycleDispatcher.onApplicationCreate(this)
+    } catch (e: Throwable) {
+      android.util.Log.e("MainApp", "AppLifecycleDispatcher failed", e)
+    }
   }
 
   override fun onConfigurationChanged(newConfig: Configuration) {
